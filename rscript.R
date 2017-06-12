7a79ad18f98d9195f3370c42649a8086427d15f8
Ben Lim <hongbeng-lim@uiowa.edu>
2017-06-13 04:02:46 +0800

    R script

---
+++ rscript.R
@@ -0,0 +1,18 @@
+setwd("~/U Iowa/kaggle")
+train <- read.csv("train.csv", header = TRUE)
+str(train)
+test <- read.csv("test.csv", header = TRUE)
+macro <- read.csv("macro.csv", header = TRUE)
+missingstrain <- data.frame(vars = names(train), missings = rep(NA, ncol(train)))
+for(i in 1:ncol(train)){
+    missingstrain$missings[i] <- sum(is.na(train[, i]))
+}
+missingstest <- data.frame(vars = names(test), missings = rep(NA, ncol(test)))
+for(i in 1:ncol(test)){
+    missingstest$missings[i] <- sum(is.na(test[, i]))
+}
+
+missingsmacro <- data.frame(vars = names(macro), missings = rep(NA, ncol(macro)))
+for(i in 1:ncol(macro)){
+    missingsmacro$missings[i] <- sum(is.na(macro[, i]))
+}
