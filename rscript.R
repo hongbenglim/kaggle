118e425574606857657e5455ab10c90a07503666
Issac Lee <issactoast@gmail.com>
2017-06-15 09:15:31 -0500

    test

--- rscript.R
+++ rscript.R
@@ -1,12 +1,14 @@
 setwd("~/U Iowa/kaggle")
+
 train <- read.csv("train.csv", header = TRUE)
-str(train)
-test <- read.csv("test.csv", header = TRUE)
+test  <- read.csv( "test.csv", header = TRUE)
 macro <- read.csv("macro.csv", header = TRUE)
+
 missingstrain <- data.frame(vars = names(train), missings = rep(NA, ncol(train)))
 for(i in 1:ncol(train)){
     missingstrain$missings[i] <- sum(is.na(train[, i]))
 }
+
 missingstest <- data.frame(vars = names(test), missings = rep(NA, ncol(test)))
 for(i in 1:ncol(test)){
     missingstest$missings[i] <- sum(is.na(test[, i]))
